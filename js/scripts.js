2adb4a65d136c9a9f89b36ba1930ee61ff321431
Taylor Somers <tsomers@yandex.com>
2020-06-04 22:32:41 -0400

    Add second question to Celebrity Dating Quiz and add if/else UI logic to scripts.js
    Co-Authored By: Beverly Potts <pottsbeverly@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-  $("form#question").submit(function(event) {
+  $("form#questions").submit(function(event) {
     const gender = $("select#gender").val();
+    const eyeColor = $("select#eye-color").val();
 
-    if (gender === "male") {
-      alert("You are male.");
-    
+    if (gender === "male" && eyeColor === "black") {
+      alert("Your Celebrity Dating Match is Janet Reno.");
+    } else if (gender === "male" && eyeColor === "white") {
+      alert("Your Celebrity Dating Match is Nicolas Cage.");
+    } else if (gender === "female" && eyeColor === "black") {
+      alert("Your Celebrity Dating Match is Gary Busey.");
     } else {
-      alert("You are female.");
+      alert("Your Celebrity Dating Match is Queen Elizabeth II.")
     }
+
   });
   event.preventDefault();
 });
